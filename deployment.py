681b613fc35bfd5f2626322a74b434e6b27ce824
Aditya vakharia <adityavakharia@gmail.com>
2024-08-25 23:04:51 +0530

    WIP

--- deployment.py
+++ deployment.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 # GitHub repository details
 repo_owner = "Aditya-rgb"  # Replace with the owner of the repository
@@ -11,7 +11,7 @@ api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits?sha={b
 
 # Time window to check for new commits (e.g., last 10 minutes)
 time_window = timedelta(minutes=10)
-time_now = datetime.utcnow()
+time_now = datetime.now(timezone.utc)  # Use timezone-aware datetime object
 
 # Fetch commits from the GitHub API
 response = requests.get(api_url)
@@ -21,6 +21,10 @@ commits = response.json()
 new_commits = []
 for commit in commits:
     commit_time = datetime.strptime(commit['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")
+    
+    # Convert the commit_time to a timezone-aware datetime in UTC
+    commit_time = commit_time.replace(tzinfo=timezone.utc)
+    
     if commit_time > (time_now - time_window):
         new_commits.append(commit)
 
